ba6d5fdd88e0f1f329e9d51801fc2f50fd817709
EepyCapy <EepyCapy@gmail.com>
2024-08-23 10:52:30 +0300

    Added examples 6-9

--- secondOrderODEs.py
+++ secondOrderODEs.py
@@ -60,3 +60,62 @@ def f6(x: float, t: float)->float:
 # Solution 6: x(t)=Ai(-t)
 def solution6(t: float | np.ndarray)->float | np.ndarray:
     return airy(-t)[0]
+
+# ODE 7 - Dampened Harmonic Oscillator
+# x'' + 2*b*x' + omega**2*x = 0, x(0)=1, x'(0)=0
+def f7(x: float, v: float, t: float)->float:
+    m = 0.5
+    s = 13.
+    r = 0.5
+    omega2 = s / m
+    b = 0.5 * r / m
+    return -2 * b * v - omega2 * x
+
+# Solution 7: x(t)=e**(-b*t)*((b/omega')*sin(omega'*t)+cos(omega'*t))
+def solution7(t: float | np.ndarray)->float | np.ndarray:
+    m = 0.5
+    s = 13.
+    r = 0.5
+    omega2 = s / m
+    b = 0.5 * r / m
+    freq = np.sqrt(omega2 - b * b) #omega'
+    return np.e**(-b * t) * (b / freq * np.sin(freq*t) + np.cos(freq*t))
+
+# ODE 8 - Forced Oscillator
+# x'' + 2*b*x' + omega**2*x = (F0/m)*cos(w*t), x(0)=1, x'(0)=0
+def f8(x: float, v: float, t: float)->float:
+    m = 0.5
+    s = 13.
+    r = 0.5
+    omega2 = s / m
+    b = 0.5 * r / m
+    F0 = 1. 
+    w = 4.
+    return -2 * b * v - omega2 * x + F0 / m * np.cos(w * t)
+
+# Solution 8
+def solution8(t: float | np.ndarray)->float | np.ndarray:
+    a = np.cos(5.07445*t)
+    b = 0.827586*np.exp(-0.5*t) - 0.0701585*np.sin(1.07445*t) - 0.00119296*np.sin(9.07445*t) + 0.150763*np.cos(1.07445*t) + 0.0216508*np.cos(9.07445*t)
+    d = np.sin(5.07445*t)
+    e = 0.0271815*np.exp(-0.5*t) + 0.150763*np.sin(1.07445*t) + 0.0216508*np.sin(9.07445*t) + 0.0701585*np.cos(1.07445*t) + 0.00119296*np.cos(9.07445*t)
+    return a * b + d * e
+
+# ODE 9 - Mass thrown upwards with air resistence
+# z'' = -g -k*z'/m, z(0)=0, z'(0)=v0
+def f9(x: float, v: float, t: float)->float:
+    k = 1.
+    m = 5.
+    g = 9.81
+    return -g - k * v / m
+
+# Solution 9
+def solution9(t: float | np.ndarray)->float | np.ndarray:
+    k = 1.
+    m = 5.
+    g = 9.81
+    v0= 200.
+    b = k / m
+    c = m * g / k
+    a = c + v0
+    return a * (1 - np.exp(-b * t)) / b - c * t
